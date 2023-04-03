93efe0c33fb44b906ce9bfe7793de97308ff276e
Alek Yoanda <alexyoandatampu1@gmail.com>
2023-04-04 01:19:42 +0700

    Fixing some error in repository

--- src/main/java/com/example/dataharianuser/repository/DataHarianDetailsRepository.java
+++ src/main/java/com/example/dataharianuser/repository/DataHarianDetailsRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface DataHarianDetailsRepository extends JpaRepository {
+public interface DataHarianDetailsRepository extends JpaRepository<DataHarianDetails, Long> {
     List<DataHarianDetails> findAll();
     List<DataHarianDetails> findAllByDataHarianId(Long dataHarianId);
     Optional<DataHarianDetails> findByDataHarianIdAndMakananId(Long dataHarianId, Long makananId);
